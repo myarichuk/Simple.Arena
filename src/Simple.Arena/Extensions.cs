471fa2f686442b1633e2749805f375a0a9c9f8d7
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-05-07 21:50:39 +0300

    add some more convenience methods

--- src/Simple.Arena/Extensions.cs
+++ src/Simple.Arena/Extensions.cs
@@ -12,10 +12,16 @@ namespace Simple.Arena
                 return new IntPtr(ptr);
         }
 
-        public unsafe static Span<T> ToSpan<T>(this IntPtr ptr) where T: unmanaged
+        public unsafe static IntPtr ToIntPtr<T>(this ReadOnlySpan<T> span) where T: unmanaged
+        {
+            fixed(T* ptr = &MemoryMarshal.GetReference(span))
+                return new IntPtr(ptr);
+        }
+
+        public unsafe static Span<T> ToSpan<T>(this IntPtr ptr, int? size = null) where T: unmanaged
         {
             byte* p = (byte*)ptr.ToPointer();
-            return MemoryMarshal.Cast<byte, T>(new Span<byte>(p, Unsafe.SizeOf<T>()));
+            return MemoryMarshal.Cast<byte, T>(new Span<byte>(p, size ?? Unsafe.SizeOf<T>()));
         }
     }
 }
